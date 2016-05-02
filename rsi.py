03b751c158188a5965a6dfaaec32c245d5dc64cf
Justin Duplessis <drfoliberg@gmail.com>
2016-05-01 22:41:44 -0400

    Wip

--- rsi.py
+++ rsi.py
@@ -17,30 +17,43 @@ def watts_lost_from(piece, temperature_delta):
     return temperature_delta / piece.rsi_value * piece.area
 
 
-def load_pieces_from_yaml(f):
+def load_yaml(f):
     pieces = []
+    sun_power = 500  # Watt/m^2
 
     with open(f, 'r') as stream:
         yaml_data = yaml.load(stream)
 
-        for piece in yaml_data:
-            area = piece['width'] * piece['length']
-            pieces.append(Piece(piece['rsi'], area))
+        power = yaml_data['collectable_width'] * \
+            yaml_data['collectable_length'] * sun_power
 
-    return pieces
+        for piece in yaml_data['pieces']:
+            if 'rsi' in piece:
+                rsi = piece['rsi']
+            elif 'rimp' in piece:
+                rsi = piece['rimp'] * 0.1761101838
+            else:
+                raise Exception('Invalid YAML')
+
+            area = piece['width'] * piece['length']
+            pieces.append(Piece(rsi, area))
+    return power, pieces
 
 
 def main():
-    total_loss_watts = 0
-    temperature_delta = 10
+    power, pieces = load_yaml('panel_2_4.yaml')
 
-    pieces = load_pieces_from_yaml('example.yaml')
+    for t in range(1, 10):
+        total_loss_watts = 0
+        temperature_delta = t * 10
 
-    for p in pieces:
-        piece_loss = watts_lost_from(p, temperature_delta)
-        total_loss_watts += piece_loss
+        for p in pieces:
+            piece_loss = watts_lost_from(p, temperature_delta)
+            total_loss_watts += piece_loss
 
-    print('Total heat loss %d Watts' % total_loss_watts)
+        efficiency = (power - total_loss_watts) / power * 100.0
+        print('Total heat loss %d Watts @ %d K delta. Eff %d%%' %
+              (total_loss_watts, temperature_delta, efficiency))
 
 if __name__ == '__main__':
     main()
